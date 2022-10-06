5e00d033770cdf715472324d3ca99ffff8b5ba92
IBashmak <ivan.bashmak@ext.us.panasonic.com>
2022-10-06 19:24:44 +0300

    fix: package-lock

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,22 @@
 import { Box } from '@mui/material';
 import { BreakdownSection } from '../components/BreakdownSection';
+import Wrapper from '../components/UI/Wrapper';
+import VerticalMenu from '../components/VerticalMenu/VerticalMenu';
+import VerticalMenu2 from '../components/VerticalMenu2/VerticalMenu2';
+import VerticalMenu3 from '../components/VerticalMenu3/VerticalMenu3';
 
 function HomePage() {
   return (
-    <Box width={850} sx={{ margin: '50px auto' }}>
-      <BreakdownSection />
-    </Box>
+    <>
+      <Box width={850} sx={{ margin: '50px auto' }}>
+        <BreakdownSection />
+      </Box>
+      <Wrapper>
+        <VerticalMenu />
+        <VerticalMenu2 />
+        <VerticalMenu3 />
+      </Wrapper>
+    </>
   );
 }
 
