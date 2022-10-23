1716357a683a0861f05c5ff4c2d533778ed9dc13
Andrea Petkova <annie.petkova@yahoo.com>
2022-10-23 17:53:17 +0300

    fix: made it more customizable by addin an outline prop to control if the label has outline or not

--- src/components/UI/Section/Section.js
+++ src/components/UI/Section/Section.js
@@ -1,11 +1,15 @@
 import { Box, Divider, Chip } from '@mui/material';
 import { styles } from './styles';
 
-export const Section = ({ title, children, sx }) => {
+export const Section = ({ title, children, sx, outline = true }) => {
   return (
     <Box sx={sx}>
       <Divider sx={styles.divider}>
-        <Chip label={title} variant='outlined' sx={styles.chip} />
+        <Chip
+          label={title}
+          variant={outline ? 'outlined' : 'string'}
+          sx={outline ? styles.chip : styles.noOutlineChip}
+        />
       </Divider>
       {children}
     </Box>
