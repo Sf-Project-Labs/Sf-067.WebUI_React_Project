830d85caff93d6e0b3b139e7e0cdb4e34af4eb20
Andrea Petkova <annie.petkova@yahoo.com>
2022-10-23 17:55:43 +0300

    fix: added Most Popular and Most Commented Section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import { Box } from '@mui/material';
 import { BreakdownSection } from '../components/BreakdownSection';
+import { MostPopularMostCommented } from '../components/MostPopularMostCommentedSection';
 
 function HomePage() {
   return (
     <Box width={850} sx={{ margin: '50px auto' }}>
       <BreakdownSection />
+      <MostPopularMostCommented />
     </Box>
   );
 }
