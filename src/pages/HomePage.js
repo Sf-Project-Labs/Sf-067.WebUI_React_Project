f2284c8b4784b72cb79fd840aff6606fdaeda69d
Andrea Petkova <annie.petkova@yahoo.com>
2022-10-28 19:02:19 +0300

    feat: added Photo Of The Day Section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,11 +3,13 @@ import { BreakdownSection } from '../components/BreakdownSection';
 import { MostPopularMostCommented } from '../components/MostPopularMostCommentedSection';
 
 import { MainArticles } from '../components/MainArticles';
+import { PhotoOfTheDay } from '../components/PhotoOfTheDay';
 function HomePage() {
   return (
     <Box width={850} sx={{ margin: '50px auto' }}>
       <MainArticles />
       <BreakdownSection />
+      <PhotoOfTheDay />
       <MostPopularMostCommented />
     </Box>
   );
