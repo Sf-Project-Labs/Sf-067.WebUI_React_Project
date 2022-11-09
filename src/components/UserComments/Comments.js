670733e856739304e24a33923312351e0af28688
RadoStef <rs_perfect@mail.bg>
2022-11-09 23:01:01 +0200

    feat: another codesmell issue removed

--- src/components/UserComments/Comments.js
+++ src/components/UserComments/Comments.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Box, Button } from '@mui/material';
 import { styles } from './styles';
 import { SortComments } from './SortComments';
